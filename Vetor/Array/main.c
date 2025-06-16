82dff78c489e275e11e32446a221cc0e1c3e3647
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-06-16 20:44:48 -0300

    programa que imprime a nota de 5 alunos usando um vetor

--- Vetor/Array/main.c
+++ Vetor/Array/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define N 5
+// arrays/vetor
 int main()
 {
-    printf("Hello world!\n");
+    float notas[N], media = 0.0;
+    int i;
+    for (i = 0; i < N; i++){
+        printf("Digite a nota do aluno %d: ", i);
+        scanf("%f", &notas[i]);
+        media = media + notas[i];
+
+    }
+    media = media / N;
+    for(i = 0; i < N; i++){
+        if(notas[i] > media)
+            printf("Aluno %d: %f\n",i, notas[i]);
+    }
+
+
+
     return 0;
 }
