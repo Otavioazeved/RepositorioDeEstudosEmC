e2b2bbbd7b9dc7077c388e8239f61c3c65605824
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-06-18 11:38:57 -0300

    array e matriz

--- Matriz/main.c
+++ Matriz/main.c
@@ -6,7 +6,9 @@ int main()
     int mat[10][5];
     int i, j;
     for(i = 0; i < 10; i++){
-        for(j = 0; j < 5; j++){
+        for(j = 0; j < 5
+        51
+        ; j++){
             printf("Digite mat[%d] [%d]", i, j);
             scanf("%d", &mat[i][j]);
 
