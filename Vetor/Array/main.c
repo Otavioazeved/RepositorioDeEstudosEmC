96ae59558d8fbece0cd6e0da17fbe16ee595867d
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-06-26 13:53:41 -0300

    somando arrays

--- Vetor/Array/main.c
+++ Vetor/Array/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main() {
-    int a, b, soma;
-printf("Digite A:");
-scanf("%d", &a);
-printf("Digite B");
-scanf("%d", &b);
-soma = a + b;
-printf("A soma de A + B: %d\n", soma);
+
+    int lista[5] = {1,2,3,4,5};
+    int soma = 0;
+    for(int i = 0; i<5; i++){
+        soma += lista[i];
+    }
+
+    printf("Soma = %d\n", soma);
+
+
 
     return 0;
 }
