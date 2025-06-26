90737cf5a443c39595c0c04a8cf2bb42867b1abf
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-06-26 13:39:56 -0300

    .

--- Vetor/Array/main.c
+++ Vetor/Array/main.c
@@ -2,21 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-   int mat[2][3];
-   for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 3; j++){
-            printf("Digite os valores na posicao: [%d] [%d]", i, j);
-            scanf("%d", &mat[i][j]);
-        }
-   }
-
-   printf("\nMatriz digitada\n");
-   for(int i = 0; i < 2; i++){
-    for(int j = 0; j < 3; j++){
-        printf("%d", mat[i][j]);
-    }
-    printf("\n");
-   }
+    int a, b, soma;
+printf("Digite A:");
+scanf("%d", &a);
+printf("Digite B");
+scanf("%d", &b);
+soma = a + b;
+printf("A soma de A + B: %d\n", soma);
 
     return 0;
 }
