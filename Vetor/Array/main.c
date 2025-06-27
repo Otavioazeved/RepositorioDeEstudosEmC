ee2ac3a6df3e6437cfb9802442bce6dc928bf2db
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-06-27 11:01:23 -0300

    vetor de strings

--- Vetor/Array/main.c
+++ Vetor/Array/main.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define tam 20
 int main() {
-    int matriz [matriz]
-    int soma = 0;
-    // ler a matriz
-    printf("Digite uma matriz de 9 numeros inteiros");
-    for(int i = 0; i<3; i++){
-        for(int j = 0; j<3; j++){
-            scanf("%d\n", &matriz[i][j]);
-        }
-    }
-
-    // imprimir a matriz fornecida pelo usuario
-    for(int i = 0; i<3; i++){
-        for(int j = 0; j<3; j++){
-            printf("%d\t", matriz[i][j]);
-        }
-        printf("\n");
-    }
-
+   char palavra [100];
+   printf("DIgite uma palavra: ");
+  fgets(palavra, 100, stdin);
+   printf("Voce digitou a palavra: %s", palavra);
 
     return 0;
 }
