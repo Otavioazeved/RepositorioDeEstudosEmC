87c59fd37f866798d278052a2d22098659274d8c
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-07-04 18:33:45 -0300

    estudando...

--- EstudandoC/main.c
+++ EstudandoC/main.c
@@ -3,11 +3,19 @@
 
 int main()
 {
-    int a, b, s;
-    printf("digite dois numeros inteiros: ");
-    scanf("%d%d", &a, &b, &s);
-    s = a + b;
-    printf("a soma entre os dois inteiros e: %d ", s);
+int m [3] [3] ={1,2,3,
+                4,5,6,
+                7,8,9};
+                int i, j, s = 0;
+                for(i = 0; i<3; i++){
+                    for(j = 0; j<3; i++){
+                        if(j == 2 - i)
+
+                            s += m [i][i] * m[i][j];
+                                printf("%d", s);
+                    }
+
+                }
 
 
     return 0;
--- Vetor/Array/main.c
+++ Vetor/Array/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main() {
     //strlen
     char  palavra [29] =  "Linguagem C";
