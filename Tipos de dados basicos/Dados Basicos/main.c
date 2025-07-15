1b05699b2516ae6e30c80cc6cd6c292ad08f6e77
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-07-15 13:41:39 -0300

    Fim da seção Dados Basicos

--- Tipos de dados basicos/Dados Basicos/main.c
+++ Tipos de dados basicos/Dados Basicos/main.c
@@ -3,7 +3,41 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    printf("Ola Mundo");
+   /* int a, b;
+    printf("Digite o primeiro numero: ");
+    scanf(" %d", &a);
+    printf("Digite o segundo numero: ");
+    scanf(" %d", &b);
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    */
+/*
+    float a;
+    printf("Digite um numero: ");
+    scanf(" %f", &a);
+    printf("seu numero na parte inteira e: %d\n", (int) a);
+    printf("parte fracionaria e: %f\n", a);
+    */
+
+    /*int n;
+    scanf("%d", &n);
+    printf(" O n = %f\n", (float) n);
+    */
+
+    // exercicio que pede ao usuario uma determinada data e o programa imprime ela em formato dd/mm/aaaa
+    int dia, mes, ano;
+    printf("Digite o dia: "); scanf("%d", &dia);
+    printf("Digite o mes: "); scanf("%d", &mes);
+    printf("Digite o ano: "); scanf("%d", &ano );
+    printf("Data completa: %d/%d/%d\n", dia, mes, ano);
+
+
+
+
+
+
+
     return 0;
 }
