9ab9e62d554a27f805f90bc13f49b9562206d9bd
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-08-17 16:27:57 -0300

    exercicio

--- Funções e Procedimentos/main.c
+++ Funções e Procedimentos/main.c
@@ -3,18 +3,33 @@
 
 //Escreva uma função que receba um número e retorne o triplo dele.
 
-int triplo(int n){
+/*int triplo(int n){
     return n * 3;
 
+}
+*/
+// função para calcular a soma de dois numeros inteiros
+
+int somarDoisInteiros(int a, int b){
+    return a + b;
 }
 
 int main()
 {
-    int v;
+    /*int v;
     printf("Digite um numero: ");
     scanf("%d", &v);
     int result = triplo(v);
     printf("o triplo de um numero n e: %d\n", result);
+    */
+
+    int v, y;
+    printf("Digite dois valores inteiros: ");
+     scanf("%d%d", &v, &y);
+    int result = somarDoisInteiros(v, y);
+    printf("A soma e: %d\n", result);
+
+
 
     return 0;
 }
